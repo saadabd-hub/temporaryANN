33462c04fb4e870f78c273d8fb982b75ac646d4c
naufalibn24 <72061940+naufalibn24@users.noreply.github.com>
2020-12-07 15:24:52 +0700

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,6 @@ import authentication from "../middlewares/authentication";
 import errorHandler from "../middlewares/errorHandler";
 import authorization from "../middlewares/authorization";
 import WebAdminRouter from "./WebAdminTodoRouter";
-<<<<<<< HEAD
-const router = Router();
-
-router.get("/", (req, res) => {
-=======
 import CommitteeRouter from "./CommitteeRoutes";
 import TournamentRouter from "./TournamentRoutes";
 import ProfileRouter from "./profile"
@@ -18,21 +13,14 @@ import ProfileRouter from "./profile"
 const router = Router();
 
 router.get("/home", (req, res) => {
->>>>>>> 226e86c
   res.send("Welcome to ANN cup");
 });
 
 router.use("", TournamentRouter);
 router.use("/user", userRouter);
-<<<<<<< HEAD
-router.use(authentication);
-router.use("/user", UnregisteredUserTodoRouter);
-
-=======
 router.use("/user", UnregisteredRouter);
 router.use("/user", ProfileRouter);
 router.use("/com", CommitteeRouter);
->>>>>>> 226e86c
 router.use("/admin", WebAdminRouter);
 
 router.use(errorHandler);
