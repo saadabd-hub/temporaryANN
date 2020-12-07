5aa840be056687ec462d0a14bb5261064df07946
naufalibn24 <72061940+naufalibn24@users.noreply.github.com>
2020-12-07 15:28:30 +0700

    Update authentication.ts

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,18 +1,4 @@
 import jwt from "jsonwebtoken";
-<<<<<<< HEAD
-require("dotenv").config();
-
-export default (req, res, next) => {
-  const { access_token } = req.headers;
-
-  if (access_token) {
-    var JWT_Accesstoken: any = process.env.JWT_Accesstoken;
-    jwt.verify(access_token, JWT_Accesstoken, (err, decoded) => {
-      if (err) next({ name: "INVALID_TOKEN" });
-      else {
-        req._id = decoded._id;
-        next();
-=======
 import User from "../models/UserModel";
 require("dotenv").config();
 
@@ -20,7 +6,6 @@ const authentication = async (req, res, next) => {
   const access_token = req.headers['x-access-token'] || req.headers['authorization'];
   if (access_token.startsWith('Bearer')) {
     try {
-
       const bearer = access_token.replace('Bearer ', '')
       const secret_key: any = process.env.JWT_Accesstoken
       const _id = await jwt.verify(bearer, secret_key);
@@ -29,7 +14,6 @@ const authentication = async (req, res, next) => {
         return res.status(401).json({
           error: "JWT token has expired, please login to obtain a new one"
         });
->>>>>>> 226e86c
       }
       const user = await User.findById(_id);
       res.locals.loggedInUser = user
