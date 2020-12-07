bf2ef5044adcd399c3b6792815567a06080d221b
naufalibn24 <72061940+naufalibn24@users.noreply.github.com>
2020-12-07 15:25:58 +0700

    Update WebAdminTodoRouter.ts

--- src/routes/WebAdminTodoRouter.ts
+++ src/routes/WebAdminTodoRouter.ts
@@ -1,22 +1,12 @@
 import Router from "express";
 import WebAdminController from "../controllers/WebAdminController";
-<<<<<<< HEAD
-=======
 import authentication from "../middlewares/authentication";
->>>>>>> 226e86c
 import authorization from "../middlewares/authorization";
 
 const router = Router();
 
-router.put(
-<<<<<<< HEAD
-  "/assign/:id",
-  authorization.webAdmin,
-  WebAdminController.assignHeadofSubdistrict
-=======
-    "/assign", authentication, authorization.allowifloggedin, authorization.grantacsess("updateOwn", "assign"),
-    WebAdminController.assign
->>>>>>> 226e86c
+router.put("/assign", authentication, authorization.allowifloggedin, authorization.grantacsess("updateOwn", "assign"),
+WebAdminController.assign
 );
 
 export default router;
