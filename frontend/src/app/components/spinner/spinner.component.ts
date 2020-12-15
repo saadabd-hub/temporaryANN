d1db718df4f0cde0087f170b7069d3e52f767c72
saadabd-hub <abdullah.sd48@gmail.com>
2020-12-15 07:05:28 +0700

    add spinner and its function

--- frontend/src/app/components/spinner/spinner.component.ts
+++ frontend/src/app/components/spinner/spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-spinner',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spinner.component.css']
 })
 export class SpinnerComponent implements OnInit {
+  dt: any; 
+  dataDisplay: any; 
+  constructor(private http: HttpClient) { 
+  } 
 
-  constructor() { }
+  ngOnInit(): void { 
+      this.http.get( 
+'/*api link*/') 
+          .subscribe(Response => { 
+                
+              // If Response comes function 
+              // hideloader() is called 
+              if (Response) { 
+                  hideloader(); 
+              } 
+              console.log(Response) 
+              this.dt = Response; 
+              this.dataDisplay = this.dt.data; 
+          }); 
+      // Function is defined 
+      function hideloader() { 
 
-  ngOnInit(): void {
-  }
-
-}
+          // Setting display of spinner 
+          // element to none 
+          document.getElementById('loading') 
+              .style.display = 'none'; 
+      } 
+  } 
+} 
